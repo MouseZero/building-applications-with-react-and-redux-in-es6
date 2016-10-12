a4f0474f69eeadd9c01f2cbab3255065555bcc0e
mousezero <rm@mousezero.com>
2016-10-12 12:17:11 -0700

    Creation of the first pages

---
+++ src/components/common/Header.js
@@ -0,0 +1,21 @@
+/**
+ * Created by logic on 10/12/16.
+ */
+import React, {PropTypes} from 'react';
+import { Link, IndexLink } from 'react-router';
+
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <IndexLink to="/" activeClassName="active">Home</IndexLink>
+        {" | "}
+        <Link to="/about" activeClassName="active">About</Link>
+        {" | "}
+        <Link to="/course" activeClassName="active">Course</Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 /**
  * Created by logic on 10/11/16.
  */
-console.log('hi');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-function hi () {
-  return 'hello there';
-}
+render (
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+);
