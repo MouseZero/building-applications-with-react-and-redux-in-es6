a89ca55705d6b542b5b40cd93f3c02b2235c705a
mousezero <rm@mousezero.com>
2016-10-11 22:46:52 -0700

    First Commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by logic on 10/11/16.
+ */
+console.log('hi');
+
+function hi () {
+  return 'hello there';
+}
---
+++ src/index.test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by logic on 10/11/16.
+ */
+import expect from 'expect';
+
+describe('Our firsttest', () => {
+  it('Should be true', () => {
+    expect(true).toEqual(true);
+  });
+});
